3c48a4da1f395923531699c68bfce6bb5c6c982f
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-06-29 23:29:52 +0100

    update

--- js/chart.js
+++ js/chart.js
@@ -2,14 +2,35 @@
 
 let canvasElem = document.getElementById('chart')
 
-/* TODO:
- * - Instantiate a new AppState
- * - Use a method on that AppState to load vote data from localStorage.
- * - Create a data object for chart.js using your AppState's allProducts array.
- * - Combine the data object with configuration information for chart.js type, colors, etc
- * - Call chart.js with the configuration and the canvasElem
- *
- */
+
+ //Instantiate a new AppState
+const appState = new AppState();
+
+//Use a method on that AppState to load vote data from localStorage.
+appState.loadVoteDataFromLocalStorage = function() {
+    let voteData = localStorage.getItem(voteData)
+    if (voteData) {
+        this.allProducts =JSON.parse(voteData)
+    }
+}
+ //Create a data object for chart.js using your AppState's allProducts array.
+For let (i = 0; i < 5; i++) {
+    let newProduct = new Product(newProducts[i].name)
+    newProduct.push(newProduct)
+}
+
+ //Combine the data object with configuration information for chart.js type, colors, etc
+ //Call chart.js with the configuration and the canvasElem
+ 
+
+
+
+
+
+
+
+
+
 function renderChart() {
 }
 
